7084455840004201dc81e4555568066edc056e4c
adamcsik <adamcsik@uniduna.hu>
2023-09-14 10:30:46 +0200

    2 szinkron

--- elso.py
+++ elso.py
@@ -1,1 +1,14 @@
-print('üdv a fedélzeten')
+
+'''print('üdv \na \nfedélzeten', end='')
+print('Szia', 'Józsi')
+'''
+
+nev = input("Mi a neved: ")
+print(f'Szia {nev}')
+print('Szia {0}'.format(nev))
+print('''Ez
+több
+sorba kerül''')
+print('Jobbra'.rjust(50,' '))
+print('Balra'.ljust(50,'-'))
+print('Közép'.center(50,' '))
